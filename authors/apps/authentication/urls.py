aacfde1f2ed2c7ba531ce811799f65a5e1be9546
arthurarty <arthur.nangai@gmail.com>
2018-11-26 19:54:46 +0300

    [ch] Adds names to routes.

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -6,6 +6,6 @@ from .views import (
 
 urlpatterns = [
     url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
-    url(r'^users/?$', RegistrationAPIView.as_view()),
-    url(r'^users/login/?$', LoginAPIView.as_view()),
+    url(r'^users/?$', RegistrationAPIView.as_view(), name='authentication'),
+    url(r'^users/login/?$', LoginAPIView.as_view(), name='login'),
 ]
